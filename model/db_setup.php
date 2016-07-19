32556951a5d1b444140b242f116eed0d657b6a4c
Tony Hansen <THansenite@gmail.com>
2016-07-19 14:51:44 -0500

    Delete db_setup.php

--- model/db_setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Information for hooking up to the database
-	$username = "DMAH3dev";
-	$password = "t1EU!FSz7";
-	$database = "DMAH3dev";
-	$server = "DMAH3dev.db.2728872.hostedresource.com";
-?>
